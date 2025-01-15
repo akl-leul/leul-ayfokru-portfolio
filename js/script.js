8e73e325633eddde0cfa4ce13403016225ff2a2c
Leul Ayfokru <abateisking@gmail.com>
2025-01-15 18:49:31 +0000

    update all

--- js/script.js
+++ js/script.js
@@ -25,38 +25,3 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         });
     });
-
-    // Typewriter Effect for .skilltext
-    const skillText = document.querySelector('.skill');
-    const text = skillText.textContent;
-    skillText.textContent = '';
-
-    let index = 0;
-
-    function typeWriter() {
-        if (index < text.length) {
-            skillText.textContent += text.charAt(index);
-            index++;
-            setTimeout(typeWriter, 100); // Adjust typing speed here
-        }
-    }
-
-    typeWriter();
-});
-
-    // Smooth Scrolling for Navigation Links
-    const navLinks = document.querySelectorAll('nav ul li a');
-
-    navLinks.forEach(link => {
-        link.addEventListener('click', (e) => {
-            e.preventDefault(); // Prevent default anchor click behavior
-            const targetId = link.getAttribute('href'); // Get the target section ID
-            const targetSection = document.querySelector(targetId); // Select the target section
-
-            // Scroll to the target section smoothly
-            targetSection.scrollIntoView({
-                behavior: 'smooth'
-            });
-
-        });
-    });
