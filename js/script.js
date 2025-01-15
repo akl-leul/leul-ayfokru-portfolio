d217d60b6aec64e2bfdc97dfe60b5ab6dcb924f1
Leul Ayfokru <abateisking@gmail.com>
2025-01-15 17:26:00 +0000

    update all

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 // Toggle Navigation Menu for Mobile View
-const navToggle = document.querySelector('.icon'); // Updated to match the button class
+document.addEventListener('DOMContentLoaded', () => {
+    const navToggle = document.querySelector('.icon'); // Updated to match the button class
 const navMenu = document.querySelector('.onMobile');
 
-navToggle.addEventListener('click', () => {
+    navToggle.addEventListener('click', () => {
   if (navMenu.style.display === 'none' || navMenu.style.display === '') {
     navMenu.style.display = 'flex'; // Show the mobile menu
   } else {
     navMenu.style.display = 'none'; // Hide the mobile menu
   }
+    });
 });
