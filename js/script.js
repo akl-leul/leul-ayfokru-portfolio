9dd52aef3acfebfd42729a64937229a528c27b8f
Leul Ayfokru <abateisking@gmail.com>
2025-01-17 13:58:19 +0000

    hah

--- js/script.js
+++ js/script.js
@@ -5,14 +5,16 @@ function showNav() {
 function closeNav(){
     document.getElementById('onMobile').style.display = 'none';
 }
-
 // Add smooth scrolling to all links
 document.querySelectorAll('a').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
+        // Scroll to the target element
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
-        });
+
+        // Hide the onMobile element when a link is clicked
         document.getElementById('onMobile').style.display = 'none';
-        });
+    });
+});
