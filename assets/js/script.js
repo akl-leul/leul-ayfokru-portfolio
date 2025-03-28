0ad9dec8157613bd2d835a5626391d73cdc47565
Leul Ayfokru <abateisking@gmail.com>
2025-03-28 08:48:32 +0000

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
 function showNav() {
     document.getElementById('onMobile').style.display = 'flex';
+    document.getElementById('icon').style.display = 'none';
 }
 
 function closeNav(){
     document.getElementById('onMobile').style.display = 'none';
+    document.getElementById('icon').style.display = 'flex';
 }
 
 // Smooth Scrolling for Navigation Links
